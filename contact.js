8e157872951da819c89a5e064ae27160f6395774
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2019-11-20 15:59:37 -0500

    switchImage Functionality on order page

--- contact.js
+++ contact.js
@@ -4,6 +4,8 @@ let thumbsUp3 = document.querySelector('.tu3');
 let thumbsUp4 = document.querySelector('.tu4');
 let thumbsUp5 = document.querySelector('.tu5');
 
+let form = document.querySelector('form');
+
 thumbsUp1.addEventListener('click', () => {
     thumbsUp1.style.color = 'green';
     thumbsUp2.style.color = 'black';
@@ -44,3 +46,27 @@ thumbsUp5.addEventListener('click', () => {
     thumbsUp5.style.color = 'green';
 });
 
+
+let opacity = 0;
+let timeInterval = 0;
+
+// fade in animation
+function fadeIn() {
+    timeInterval = setInterval(show,20);
+}
+
+// slowly increases the opacity of the body
+function show() {
+    opacity = Number(window.getComputedStyle(form).getPropertyValue('opacity'));
+    if(opacity < 1){
+        // edit the + [value] to change rate at which the body fades in 
+        opacity = opacity + 0.007;
+        form.style.opacity = opacity;
+    }
+    else {
+        clearInterval(timeInterval);
+    }
+}
+
+form.style.opacity = 0;
+fadeIn();
