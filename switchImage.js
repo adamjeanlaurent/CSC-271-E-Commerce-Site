fc5ca5b131ce593250700ad8e24d0d246a58e3ae
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2019-11-21 22:20:44 -0500

    documentation

--- switchImage.js
+++ switchImage.js
@@ -1,3 +1,8 @@
+/*
+    This file control the switching of the pictures of the differnt products that are clickable 
+    by adding an event listener to each one that changes the target's src attribute to that of the main image
+*/
+
 let currentPillow = document.querySelector('.currentPillow');
 let pillows = document.querySelectorAll('.pillow');
 
